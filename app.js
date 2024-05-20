ae3b441531105ac3e6298d97f014be6d30be9937
Aleksa Piperski <mr.alexpiperski@gmail.com>
2024-05-20 15:03:15 +0200

    Version 1.2(get/post testing)

--- app.js
+++ app.js
@@ -1,16 +1,38 @@
-const express = require('express');
+const fs = require("fs");
+const express = require("express");
 
 const app = express();
 
-app.get('/',(req,res)=>{
-  res.status(200).json({message: 'Hello from the server',app:'Natours'});
+app.use(express.json()); //middleware
+
+// app.get('/',(req,res)=>{
+//   res.status(200).json({message: 'Hello from the server',app:'Natours'});
+// });
+
+// app.post('/',(req,res)=>{
+//   res.send('You can post to this endpoint...')
+// })
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get("/api/v1/tours", (req, res) => {
+  res.status(200).json({
+    status: 'sucsess',
+    results: tours.length,
+    data: {
+      tours: tours  // path tours and const tours object , could write only tours
+    }
+  })
 });
 
-app.post('/',(req,res)=>{
-  res.send('You can post to this endpoint...')
+app.post("/api/v1/tours",(req,res)=>{
+  console.log(req.body);
+  res.send('Done');
 })
 
 const port = 3000;
-app.listen(port, ()=>{
+app.listen(port, () => {
   console.log(`Listening on the port:${port} for a request...`);
-})
+});
