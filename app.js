5f32b89f2799f88d0d77a38a80536b4bd6699bac
Aleksa Piperski <mr.alexpiperski@gmail.com>
2024-05-25 15:32:52 +0200

    Version 2.7(added config env)

--- app.js
+++ app.js
@@ -5,10 +5,14 @@ const userRouter = require("./routes/userRoutes");
 
 const app = express();
 
-//MIDDLEWARE
+// MIDDLEWARE
+if(process.env.NODE_ENV === 'development'){
+  app.use(morgan("dev"));
+};
+
 app.use(express.json());
 
-app.use(morgan("dev"));
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log("Hello from the custom middleware!");
