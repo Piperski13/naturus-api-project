9aba829f9968fbe682b808fc0cbc98411832746e
Aleksa Piperski <mr.alexpiperski@gmail.com>
2024-05-25 23:21:10 +0200

    Verion 3.6(added delete tour mongoose findIdAndDelete()

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -74,9 +74,17 @@ exports.updateTour = async(req, res) => {
     });
   };
 };
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: "sucsess",
-    data: null,
-  });
+exports.deleteTour = async(req, res) => {
+  try {
+    await Tour.findByIdAndRemove(req.params.id)
+    res.status(204).json({
+      status: "sucsess",
+      data: null,
+    });
+  } catch (error) {
+      res.status(400).json({
+        status: 'failed request',
+        message: error
+      });
+  }
 };
