a452589c5cbb7706ad15e849f22d69b6ec51b4c7
Aleksa Piperski <mr.alexpiperski@gmail.com>
2024-06-25 15:53:16 +0200

    Version 3.8 ( added script for importing/deleting DB )

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,54 @@
+const fs = require('fs')
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+const Tour = require('../../models/tourModel');
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Succesfull DB conection"))
+  .catch((err) => console.error("DB connection error:", err));
+
+//READ JSON FILE
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours-simple.json`,'utf-8'));
+
+//IMPORT DATA INTO DB
+const importData = async() =>{
+  try {
+    await Tour.create(tours);
+    console.log('Database sucessfully loaded!');
+  } catch (error) {
+    console.log(error);
+  }
+  process.exit();
+}
+
+//DELETE DATA FROM DB
+const deleteData = async() =>{
+  try {
+    await Tour.deleteMany();
+    console.log('Database sucessfully deleted!');
+  } catch (error) {
+    console.log(error);
+  }
+  process.exit();
+}
+
+if(process.argv[2] === '--import'){
+  importData();
+}
+else if (process.argv[2] === '--delete'){
+  deleteData();
+}
+
+console.log(process.argv);
